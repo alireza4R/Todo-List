37fefb165d27259c0e27314959d03dc6c88ff809
Alireza <rostmi83@gmail.com>
2025-03-30 11:43:36 +0330

    delete example package and add todolist package

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,11 @@
 import java.util.ArrayList;
 import db.*;
 import db.exception.InvalidEntityException;
-import example.*;
 
 
 
 public class Main {
     public static void main(String[] args) throws InvalidEntityException, CloneNotSupportedException {
-        Document doc = new Document("Eid Eid Eid");
-
-        Database.add(doc);
-
-        System.out.println("Document added");
-
-        System.out.println("id: " + doc.id);
-        System.out.println("content: " + doc.content);
-        System.out.println("creation date: " + doc.getCreationDate());
-        System.out.println("last modification date: " + doc.getLastModificationDate());
-        System.out.println();
-
-        try {
-            Thread.sleep(3_000);
-        } catch (InterruptedException e) {
-            System.out.println("Sleep interrupted!");
-        }
-
-        doc.content = "This is the new content";
-
-        Database.update(doc);
-
-        System.out.println("Document updated");
-        System.out.println("id: " + doc.id);
-        System.out.println("content: " + doc.content);
-        System.out.println("creation date: " + doc.getCreationDate());
-        System.out.println("last modification date: " + doc.getLastModificationDate());
 
 
     }
